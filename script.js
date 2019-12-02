4b5d9be0dc680f1cfc27570973d9f18b1fb4142a
Keianna17 <56314410+Keianna17@users.noreply.github.com>
2019-12-02 12:46:25 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,28 @@
+
+var activity = "Friday";
+switch (activity){
+    case 'Monday':
+        console.log(" So tired , sleep deprived");
+    break;
+    case 'Tuesday':
+        console.log("Ehhh I can do it");
+    break;
+    case 'Wednesday':
+        console.log("Half way and its an early day!!!!");
+    break;
+    case 'Thursday':
+        console.log("Almost party time");
+    break;
+    case 'Friday':
+        console.log("Its FRIDAY TURNUPP");    
+    break;
+    case 'Saturday':
+        console.log("Recover day ");
+    break ;
+    case 'sunday':
+        console.log ("Its back to hell ");
+    break; 
+    default:
+    console.log ("UMMM what day is it ");
+    
+    }
